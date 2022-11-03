370f40b824cf8995e71d3109123add069b4f8c04
Lukas Panni <lukas.panni@outlook.de>
2022-11-03 09:05:17 +0100

    Add powershell profile

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,5 @@
+oh-my-posh --init --shell pwsh --config 'C:/Users/Lukas Panni/AppData/Local/Programs/oh-my-posh/themes/craver.omp.json' | Invoke-Expression
+Set-Alias lg lazygit
+Set-Alias g git
+Set-Alias vim nvim
+function zsh { wsl.exe zsh }
