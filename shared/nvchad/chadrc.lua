7e9be297d47b73965bafdbf24ccf726ea24cc84c
Lukas Panni <lukas.panni@outlook.de>
2023-07-13 17:48:55 +0200

    Use pwsh on windows

--- shared/nvchad/chadrc.lua
+++ shared/nvchad/chadrc.lua
@@ -4,4 +4,13 @@ M.ui = {
     theme = 'github_dark'
 }
 M.plugins = "custom.plugins"
+
+
+local windows = vim.fn.has("win32") == 1
+if windows then
+  vim.opt.shell = 'pwsh'
+  vim.opt.shellcmdflag = '-nologo -noprofile -ExecutionPolicy RemoteSigned -command'
+  vim.opt.shellxquote = ''
+end
+
 return M
