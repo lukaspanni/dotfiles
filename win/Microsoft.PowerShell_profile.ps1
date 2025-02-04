312e4318f5ec4e39482cc3ea58dec12138883ed4
Lukas Panni <lukas.panni@outlook.de>
2025-02-04 16:29:47 +0100

    Update powershell profile, refactor powershell setup

--- win/Microsoft.PowerShell_profile.ps1
+++ win/Microsoft.PowerShell_profile.ps1
@@ -5,9 +5,33 @@ Set-Alias g git
 Set-Alias vim nvim
 Set-Alias htop ntop 
 Set-Alias which gcm
+Set-Alias y yazi
 
 $VServer="37.120.173.24"
 $dev = "E:\Dokumente\Development"
 
 function zsh { wsl.exe zsh }
+
+# Fix winget paths
+$wingetPackagesPath = "$env:LOCALAPPDATA\Microsoft\WinGet\Packages"
+$packageDirs = Get-ChildItem -Directory -Path $wingetPackagesPath
+
+function IsPathInEnvironment {
+    param (
+        [string]$path
+    )
+    $envPath = $env:Path
+    return $envPath.Split(';') -contains $path
+}
+
+foreach ($packageDir in $packageDirs) {
+    # Only consider subfolders that contain binaries to be added to path
+    $binaryFiles = Get-ChildItem -Path $packageDir.FullName -Recurse -Include *.exe
+
+    if ($binaryFiles) {
+        if (-not (IsPathInEnvironment -path $packageDir.FullName)) {
+            $env:Path += ";$($packageDir.FullName)"
+        }
+    }
+}
 
--- win/setup.ps1
+++ win/setup.ps1
@@ -5,39 +5,40 @@ function Install-Packages {
 
   $programs_winget = @(
     "Canonical.Ubuntu2404",
+    "Derailed.k9s",
     "Docker.DockerDesktop",
     "Git.Git",
     "GitHub.cli",
+    "GoLang.Go",
     "Google.Chrome",
-    "gsass1.NTop",
     "JanDeDobbeleer.OhMyPosh",
-    "JesseDuffield.lazydocker", 
-    "JesseDuffield.lazygit", 
-    "Microsoft.DotNet.SDK.8",
+    "JesseDuffield.lazydocker",
+    "JesseDuffield.lazygit",
+    "Microsoft.DotNet.SDK.9",
     "Microsoft.PowerShell",
     "Microsoft.PowerToys",
+    "Microsoft.VisualStudioCode",
     "Microsoft.WindowsTerminal",
     "Mozilla.Firefox",
+    "Neovim.Neovim",
     "Notepad++.Notepad++",
     "Obsidian.Obsidian",
-    "Oven-sh.Bun",
     "OpenJS.NodeJS",
+    "Oven-sh.Bun",
     "Python.Python.3.13",
     "gerardog.gsudo",
-    "Microsoft.VisualStudioCode",
-    "Neovim.Neovim",
-    "GoLang.Go"
+    "gsass1.NTop",
+    "sxyazi.yazi"
   )
 
   $optional_winget = @(
-    "IDRIX.VeraCrypt",
     "Duplicati.Duplicati",
+    "IDRIX.VeraCrypt",
     "Insecure.Nmap",
-    "WiresharkFoundation.Wireshark",
     "MiKTeX.MiKTeX",
     "Microsoft.OpenJDK.21",
     "VideoLAN.VLC",
-    "Notion.Notion"
+    "WiresharkFoundation.Wireshark"
   )
 
   $all_programs = $programs_winget
@@ -55,18 +56,21 @@ function Install-Packages {
   }
 }
 
+function Copy-Dotfiles {
+  $cwd = pwd
+  cd $PSScriptRoot
+  cp -r ../shared/.git* $HOME
+  cp -r ../shared/nvim $env:localappdata/nvim
+  cp ./Microsoft.PowerShell_profile.ps1 $PROFILE
+  cd $cwd
+}
 function Configure {
   try {
-    $cwd = pwd
-    cd $PSScriptRoot
-    cp -r ../shared/.git* $HOME
-    cp -r ../shared/nvim $env:localappdata/nvim
+    Copy-Dotfiles
     Write-Host -NoNewLine 'To complete nvim setup, run nvim and execute :MasonInstallAll
     Press any key to continue...';
     $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
     nvim
-    cp ./Microsoft.PowerShell_profile.ps1 $PROFILE
-    cd $cwd
   }
   catch {
     Write-Host "Configuration failed: $_"
@@ -80,6 +84,9 @@ if ($args[0] -eq "install" -or $args[0] -eq "") {
 elseif ($args[0] -eq "install-full") {
   Install-Packages -FullInstall:$true
   Configure
+}
+elseif($args[0] -eq "update-dotfiles") {
+  Copy-Dotfiles
 }
 elseif ($args[0] -like "config*") {
   Configure
